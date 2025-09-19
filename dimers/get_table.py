12f81be142ae32499d9e927df4f2bf32adbe5f70
Kazi Shudipto Amin <rhetorix@gmail.com>
2025-09-19 15:56:33 +0200

    Closes #31 by implementing csv write function

--- dimers/get_table.py
+++ dimers/get_table.py
@@ -10,9 +10,30 @@ import glob
 import ast
 
 parser = argparse.ArgumentParser(description="Get table of energies")
-parser.add_argument("outputs_path", help="Path to the folder containing output files; e.g., dimers/xg_interation_outputs/")
-parser.add_argument("--metadata", help="Optional path to metadata file (csv or json). Defaults to 'metadata.json' in the outputs_path.", default=None, required=False)
-
+parser.add_argument(
+    "outputs_path", 
+    help="Path to the folder containing output files; e.g., dimers/xg_interation_outputs/"
+)
+parser.add_argument(
+    "--metadata", 
+    help="Optional path to metadata file (json). Defaults to 'metadata.json' in the outputs_path.", 
+    default=None, required=False
+)
+parser.add_argument(
+    "-o", "--outfile", 
+    help="Optional path to write to (csv). By default generated from metadata and placed in outputs directory.",
+    default=None, required=False
+)
+parser.add_argument(
+    "-s", "--suffix", 
+    help="Optional suffix for generated filename. Default is 'all'",
+    default='all', required=False
+)
+parser.add_argument(
+    "-p", "--print_only", 
+    help="Dry run",
+   action="store_true", required=False
+)
 
 def parse_output_to_df(output_str: str) -> pd.DataFrame:
     """
@@ -29,7 +50,7 @@ def parse_output_to_df(output_str: str) -> pd.DataFrame:
         Dataframe with 'DISTANCES' (float) and 'E' (np.float64).
     """
     # Step 1: Read the string into a DataFrame
-    df = pd.read_csv(StringIO(output_str), sep='\s+')
+    df = pd.read_csv(StringIO(output_str), sep=r'\s+')
 
     # Step 2: Extract distances from labels (float after second 'r_')
     distances = []
@@ -98,8 +119,11 @@ def get_cmd_from_metadata(metadata: dict, outputs_path: str):
     files_str = ' '.join(files)
     cmd = f"python {tabulate_script} {cmd_arg} {files_str}"
     return cmd
-    
-  
+
+def get_default_output_fname(metadata, ext='csv', suffix='all'):
+    fname = "{prefix}.{ext}".format(**metadata, ext=ext).format(distance = suffix)
+    return fname
+
 def main(args):
     # Save current working directory
     metadata_path = args.metadata or os.path.join(args.outputs_path, 'metadata.json')
@@ -115,12 +139,23 @@ def main(args):
         text=True,
         check=True
     )
-    
-    
+   
     output_str = result.stdout
     df = parse_output_to_df(output_str)
-    print(df.to_string())
-    #fname=f"inte_{args.atom1}_{args.atom2}_vs_r_1.00_1.40_1.80.csv"
+
+
+    if not args.outfile:
+        fname = os.path.join(
+            args.outputs_path,get_default_output_fname(metadata, suffix=args.suffix)
+        )
+    else:
+        fname = args.outfile
+
+    if args.print_only:
+        print(f"Dry-run, would write to `{fname}`:")
+        print(df.to_string())
+        return None
+    df.to_csv(fname, index=False)
     #print(df.to_csv(fname, index=False))
     
 if __name__ == "__main__":
